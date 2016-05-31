2f803c0fdfd090497290e04bad92968689c3afe6
Franco <francosebastian22@gmail.com>
2016-05-31 16:18:41 -0400

    El carro de compras agrega productos perfectamente

--- AlimentosSantiago.Web/Productos/Producto.aspx.cs
+++ AlimentosSantiago.Web/Productos/Producto.aspx.cs
@@ -6,11 +6,14 @@ using System.Web;
 using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using AlimentosSantiago.Web.WebUtils;
+using AlimentosSantiago.Dto;
 
 namespace AlimentosSantiago.Web.Productos
 {
-    public partial class Producto : System.Web.UI.Page
+    public partial class Producto : PaginaBase
     {
+        public int ViewStateIdProducto { get { return (int)ViewState["ViewStateIdProducto"]; } set { ViewState["ViewStateIdProducto"] = value; } }
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -22,13 +25,22 @@ namespace AlimentosSantiago.Web.Productos
         {
             using (OracleDbContext db = new OracleDbContext())
             {
-                return db.Plato.SingleOrDefault(t => t.Id == id);
+                
+                Plato plato = db.Plato.SingleOrDefault(t => t.Id == id);
+                this.ViewStateIdProducto = plato.Id;
+                return plato;
             }
         }
 
         protected void BtnAgregar_Click(object sender, EventArgs e)
         {
+            using (OracleDbContext db = new OracleDbContext())
+            { 
+                Plato plato = db.Plato.SingleOrDefault(t => t.Id == this.ViewStateIdProducto);
+                CarritoCompras.Instance.AddItem(plato);
 
+            }
+            MostrarMensaje("Producto añadido al carrito de compras");         
         }
     }
 }
